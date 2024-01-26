a2d7780060859bc3470eb437443ad982c8e50cea
Shirin <shirin@organizeapp.net>
2024-01-26 14:46:17 -0500

    top progress bar responsive to the progress

--- app/(components)/(createEventForm)/StepA.EventName.jsx
+++ app/(components)/(createEventForm)/StepA.EventName.jsx
@@ -1,8 +1,14 @@
-const EventName = () => {
+const EventName = ({ handleChangeInput }) => {
     return (
         <div className="q-title flex flex-col items-center  h-screen mx-5 m-10">
             <h3 className="mb-4">What is your event name?</h3>
-            <input type="text" id="eventName" name="eventName" className="w-full  py-2  text-border-3 rounded text-blue-600" required />
+            <input
+                type="text"
+                id="eventName"
+                name="eventName"
+                className="w-full  py-2  text-border-3 rounded text-blue-600" required
+                onChange={handleChangeInput}
+            />
         </div>
     );
 };
--- app/(components)/ProgressBar.jsx
+++ app/(components)/ProgressBar.jsx
@@ -1,12 +1,10 @@
-import { faTicketSimple } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-
-const ProgressBar = ({ progress }) => {
+const ProgressBar = ({ step, totalSteps }) => {
+    const progress = (step / totalSteps) * 100;//Percentage of the progress
+    console.log("we are on step", progress);
     return (
         <div className="w-full bg-gray-200 rounded-full h-5 dark:bg-blue-400">
             <div className="bg-blue-600 h-5 rounded-full"
-                style={{ width: `${progress}0%` }}>
+                style={{ width: `${progress}%` }}>
 
             </div>
         </div >
