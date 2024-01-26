a087edbbe6acc9d0d2751f6510564a370f8d150b
Shirin <shirin@organizeapp.net>
2024-01-26 16:34:59 -0500

    finished functionality of event name page

--- app/(components)/(createEventForm)/StepA.EventName.jsx
+++ app/(components)/(createEventForm)/StepA.EventName.jsx
@@ -1,4 +1,4 @@
-const EventName = ({ handleChangeInput }) => {
+const EventName = ({ callBack }) => {
     return (
         <div className="q-title flex flex-col items-center  h-screen mx-5 m-10">
             <h3 className="mb-4">What is your event name?</h3>
@@ -6,8 +6,8 @@ const EventName = ({ handleChangeInput }) => {
                 type="text"
                 id="eventName"
                 name="eventName"
-                className="w-full  py-2  text-border-3 rounded text-blue-600" required
-                onChange={handleChangeInput}
+                className="text-field w-full  py-2  text-border-3 rounded text-blue-600" required
+                onChange={callBack}
             />
         </div>
     );
