8333cacc7865b7db987d011f448b1b5a329a3e5f
Shirin <shirin@organizeapp.net>
2024-01-30 17:41:58 -0500

    capturing data on organizer Info Page

--- app/(components)/(createEventForm)/StepD.GetUserInfo.jsx
+++ app/(components)/(createEventForm)/StepD.GetUserInfo.jsx
@@ -1,47 +1,61 @@
 "use Client";
-const GetUserInfo = () => {
+import React, { useState, useEffect } from 'react';
 
-  const handleChange = () => {
-    console.log("contact info changed ");
+const GetUserInfo = ({ callBack, organizerData = [] }) => {
+  const [organizerInfo, setOrganizerInfo] = useState(organizerData);
+
+  useEffect(() => {
+    console.log("organizer info is: ", organizerInfo);
+    callBack(organizerInfo);
+  }, [organizerInfo]);
+
+  const handleChange = (e) => {
+    setOrganizerInfo({
+      ...organizerInfo,
+      [e.target.name]: e.target.value,
+    });
   };
+
 
   return (
     <div>
-      <h1>
+      <div>
+        <h1>
+          Last step, what is your name and phone number?
+        </h1>
+        <label className="block mb-2 text-sm font-bold text-gray-600">First Name</label>
+        <input
+          className="w-full p-2 border rounded-md"
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={organizerInfo.firstName}
+          required
+          onChange={handleChange}
+        />
 
-        Last step, what is your name and phone number?
-      </h1>
+        <label className="block mt-4 mb-2 text-sm font-bold text-gray-600">Last Name</label>
+        <input
+          className="w-full p-2 border rounded-md"
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={organizerInfo.lastName}
+          required
+          onChange={handleChange}
+        />
 
-      <label className="block mb-2 text-sm font-bold text-gray-600">First Name</label>
-      <input
-        type="text"
-        name="firstName"
-        value={""}
-
-        className="w-full p-2 border rounded-md"
-        required
-      />
-
-      <label className="block mt-4 mb-2 text-sm font-bold text-gray-600">Last Name</label>
-      <input
-        type="text"
-        name="lastName"
-        value={""}
-
-        className="w-full p-2 border rounded-md"
-        required
-      />
-
-      <label className="block mt-4 mb-2 text-sm font-bold text-gray-600">Phone Number</label>
-      <input
-        type="tel"
-        name="phoneNumber"
-        value={""}
-
-        className="w-full p-2 border rounded-md"
-        required
-      />
-
+        <label className="block mt-4 mb-2 text-sm font-bold text-gray-600">Phone Number</label>
+        <input
+          className="w-full p-2 border rounded-md"
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={organizerInfo.phoneNumber}
+          required
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 };
