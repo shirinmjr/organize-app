917bec8d84da6f8b6d4fe64ae0aa73f2792b3cdb
Shirin <shirin@organizeapp.net>
2024-01-30 15:22:27 -0500

    finsihed add questions functin first draft

--- app/(components)/(createEventForm)/StepA.EventName.jsx
+++ app/(components)/(createEventForm)/StepA.EventName.jsx
@@ -1,12 +1,13 @@
-const EventName = ({ callBack }) => {
+const EventName = ({ callBack, eventName = "" }) => {
     return (
         <div className="q-title flex flex-col items-center  h-screen mx-5 m-10">
             <h3 className="mb-4">What is your event name?</h3>
             <input
+                className="text-field w-full  py-2  text-border-3 rounded text-blue-600" required
                 type="text"
                 id="eventName"
                 name="eventName"
-                className="text-field w-full  py-2  text-border-3 rounded text-blue-600" required
+                value={eventName}
                 onChange={callBack}
             />
         </div>
