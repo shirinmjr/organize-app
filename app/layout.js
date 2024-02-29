894d995ab39994901542d993f7be1206634e6c97
Shirin <shirin@organizeapp.net>
2024-02-28 19:42:08 -0500

    text field style

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@ import { Inter } from "next/font/google";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import GetStarted from "./components/GetStarted";
 
 config.autoAddCss = false;
 
@@ -24,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-col h-screen max-h-screen">
+        <div className="flex flex-col h-screen max-h-screen max-w-screen-lg">
           <div className="flex-grow overflow-y-auto bg-page text-default-text">
             {children}
 
