cdafd566651a6ec4e1377d4649760cff7b0be3f3
Shirin Mohajer <shirin@Shirins-MacBook-Pro.local>
2024-03-09 09:37:10 -0500

    switchYes component created

--- app/components/createEventForm/StepA.EventName.jsx
+++ app/components/createEventForm/StepA.EventName.jsx
@@ -1,3 +1,4 @@
+import SwitchYes from "../inputs/SwitchYes";
 const EventName = ({ callBack, eventName = "" }) => {
   return (
     <div className="event-form">
@@ -11,18 +12,7 @@ const EventName = ({ callBack, eventName = "" }) => {
         value={eventName || ""}
         onChange={callBack}
       />
-      <div class="flex items-center mx-2">
-        <h3>I Have a date </h3>
-        <label
-          for="check"
-          className="bg-gray-100 w-20 h-10 cursor-pointer relative m-3 rounded-full"
-        >
-          <input type="checkbox" id="check" className="sr-only peer" />
-          <span className="w-2/4 h-4/5 text-white bg-blue-200 absolute rounded-full left-1 top-1 peer-checked:bg-brandBlue peer-checked:left-10 transition-all duration-500 items-center p-1">
-            Yes
-          </span>
-        </label>
-      </div>
+      <SwitchYes label={"I have a date"} />
     </div>
   );
 };
