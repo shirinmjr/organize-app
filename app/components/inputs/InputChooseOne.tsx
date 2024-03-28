c7e5a916188934a297626a83845b9746b48e83d3
Raffaele Cataldo <kemanicataldo@gmail.com>
2024-03-28 12:43:11 -0700

    add prettier

--- app/components/inputs/InputChooseOne.tsx
+++ app/components/inputs/InputChooseOne.tsx
@@ -9,9 +9,7 @@ import { IQuestionOption } from "@/lib/types";
 
 function mapIndexToLetterList(index: number): string {
   if (index > 25) {
-    console.warn(
-      "Can only map lowercase letters of the alphabet, now starting over from the beginning"
-    );
+    console.warn("Can only map lowercase letters of the alphabet, now starting over from the beginning");
   }
   return `${((index % 26) + 10).toString(36)}.`;
 }
@@ -24,13 +22,7 @@ interface InputChooseOneProps {
   onChange?: (arg0: string | number) => void;
 }
 
-const InputChooseOne = ({
-  label,
-  name,
-  options,
-  value = "",
-  onChange = undefined,
-}: InputChooseOneProps) => {
+const InputChooseOne = ({ label, name, options, value = "", onChange = undefined }: InputChooseOneProps) => {
   const [choice, setChoice] = useState(value);
 
   const handleChoose = (value: string | number) => {
@@ -48,28 +40,13 @@ const InputChooseOne = ({
               <div
                 className={`
 flex justify-between p-3 my-2 border border-blue-400 rounded-full cursor-pointer  border-1 focus:ring-none hover:border-blue-700
-            ${
-              active
-                ? "ring-1 ring-white/60 ring-offset-1 border-blue-700 focus:outline-blue-700"
-                : ""
-            }
-            ${
-              checked
-                ? "bg-white border-blue-700 font-bold text-blue-600"
-                : "bg-blue-50"
-            }`}
-              >
+            ${active ? "ring-1 ring-white/60 ring-offset-1 border-blue-700 focus:outline-blue-700" : ""}
+            ${checked ? "bg-white border-blue-700 font-bold text-blue-600" : "bg-blue-50"}`}>
                 <span>
                   <span className="mr-2">{mapIndexToLetterList(index)}</span>
                   <span>{option.option}</span>
                 </span>
-                {checked ? (
-                  <FontAwesomeIcon
-                    className="self-center mr-2"
-                    icon={faCheck}
-                    aria-hidden="true"
-                  />
-                ) : null}
+                {checked ? <FontAwesomeIcon className="self-center mr-2" icon={faCheck} aria-hidden="true" /> : null}
               </div>
             )}
           </RadioGroup.Option>
